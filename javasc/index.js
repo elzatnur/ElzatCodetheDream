18093a5b3ab3c2d92a1f8a8aabe138c52cb40c06
elzatnur <elzat.nur86@gmail.com>
2024-08-21 13:35:43 -0400

    updated lesson-13

--- javasc/index.js
+++ javasc/index.js
@@ -1,15 +1,16 @@
-// const skill = ["Networking", "Java", "Automation"];
-// console.log(skill);
-// const getSkills = document.querySelector('#section-skills');
-// console.log(getSkills);
-// var skillList = document.getElementsByClassName(".skills");
-// console.log(skillList);
-// for(let i=0; i<skill.length; i++){
-//   var s= document.createElement("li");
-//   s.innerText= skill(i);
-//   console.log(s);
-//   skillList.appendChild(s);
-//   console.log(skillList);
+const skill = ["Networking", "Java", "Automation"];
+console.log(skill);
+const getSkills = document.querySelector('#section-skills');
+console.log(getSkills);
+var skillList = document.querySelector(".skills")
+console.log(skillList);
+for(let i=0; i<skill.length; i++){
+  var s= document.createElement("li");
+  s.innerText= skill[i];
+  console.log(s);
+  skillList.appendChild(s);
+  console.log(skillList);
+}
 
 // create a footer element
 // const footer = document.createElement('footer');
@@ -19,9 +20,10 @@
 
 // const body = document.querySelector('body');
 // body.appendChild(footer);
-
+const body = document.querySelector("body");
 const footer = document.createElement('footer');
 footer.className = "foot";
+body.appendChild(footer);
 console.log(footer);
 const getFoot = document.querySelector('.foot');
 const today = new Date();
@@ -32,11 +34,11 @@ var copyRightText = copyrightSymbol + "Ela Nur" + year;
 getFoot.innerHTML= copyRightText;
 
 
-const tag = document.querySelector(".skills")
-console.log(tag);
-const sum = document.createElement("li");
-sum.innerText= "Networking"
-tag.appendChild(sum);
+// const tag = document.querySelector(".skills")
+// console.log(tag);
+// const sum = document.createElement("li");
+// sum.innerText= "Networking"
+// tag.appendChild(sum);
 
 
 
